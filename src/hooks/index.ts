ea9beccd7d2ad5da71f38300a8ea0987c364d151
arshad-shah <49516613+arshad-shah@users.noreply.github.com>
2025-12-05 16:19:29 +0000

    Build: (c989dce) fix: add write permissions for GitHub Actions
    - Add contents: write permission to allow pushing to build branch
    - Required for git-publish-subdir-action to deploy

--- src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// src/hooks/index.ts
-
-export * from './useMediaQuery'
-export * from './useScrollDirection'
-export * from './useIntersectionObserver'
-export * from './usePreferredMotion'
-export * from './useClickOutside'
-export * from './useKeyPress'
-export * from './useCopyToClipboard'
